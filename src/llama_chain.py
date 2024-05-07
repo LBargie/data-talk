38a5ca69ec72b9fd0b75a13ed98140f1a8a827c7
Lee Barrett <90680419+LBargie@users.noreply.github.com>
2024-05-07 22:51:22 +0100

    fix llama-index sql database import

--- src/llama_chain.py
+++ src/llama_chain.py
@@ -1,5 +1,4 @@
-# from llama_index.core import SQLDatabase as llamadb
-import llama_index.core
+from llama_index.core import SQLDatabase
 from llama_index.llms.huggingface import HuggingFaceInferenceAPI
 from llama_index.core.retrievers import NLSQLRetriever
 from src.common import LanguageChain
@@ -10,7 +9,7 @@ class LlamaChain(LanguageChain):
     def __init__(self, model: str, db_engine: Engine, **kwargs):
         super().__init__(model, db_engine)
         self.llm = HuggingFaceInferenceAPI(model_name=self.model, **kwargs)
-        self.db = llama_index.core.SQLDatabase(
+        self.db = SQLDatabase(
             self.db_engine, include_tables=["llm_table"]
         )
 
